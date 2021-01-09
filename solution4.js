21755deccc940281f8ebf14dfa232bb9188ead40
Dylan Crawshaw <dcrawsh@gmail.com>
2021-01-08 18:19:48 -0800

    clean up solutions and submit

--- solution4.js
+++ solution4.js
@@ -2,8 +2,8 @@ $.each(window.iceCreamFlavors, (index,value) =>  {
         let item = document.createElement('li');
         item.innerText = value;
         $('.ice-cream-list').prepend(item);
-        $(item).css("display","none");
-        $(item).addClass("ice-cream-flavor");
+        $(item).addClass(`ice-cream-flavor ${index}`);
+        $(item).hide()
    }); 
 $('body').append("<button>SHOW ME THE ICE CREAM</button>")
 $('button').css({"background-color":"white","border-radius":"25px","padding":"2rem","border":"2px solid #008CBA"})
@@ -11,7 +11,9 @@ $('button').css({"background-color":"white","border-radius":"25px","padding":"2r
 
 
 $('button').click(function (e) { 
-        
+        $('li').show(1000);
+        $('body').prepend('<h1>A little more jQuery fun</h1>');
+
 });
 $('button').hover(function (e) { 
         $(this).css({"background-color":"#008CBA","color":"white"});  
